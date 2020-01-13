9752ff0963052d0491f41ce62a52357d05f45d4d
Chris Aragon <aragonchristopherray@gmail.com>
2020-01-13 13:43:39 -0500

    why do images not render

--- dog_app_1.js
+++ dog_app_1.js
@@ -1,32 +1,28 @@
 'use strict'
 
-
-function fetchDogImage(){
-        fetch('https://dog.ceo/api/breeds/image/random/' + number, 3)
-        .then(response => response.json())
-        .then(responseJson => console.log(responseJson))
-        .catch(alert("Ruh_Roh! Something went wrong! Maybe try again later"));
+function fetchDogImage(number){
+    fetch('https://dog.ceo/api/breeds/image/random/' + number)
+    .then(response => response.json())
+    .then(responseJson => console.log(responseJson))
+    .then(responseJson => displayDogImage(responseJson))    
+    .catch(alert("Ruh_Roh! Something went wrong! Maybe try again later"));
 }
-
-
 
 function storeNumber(){
     $(".submit").click(function(event){
         event.preventDefault();
-        let number = $(".number").val()
+        let number = $(".number").val() === "" ? 3 : $(".number").val();
         console.log(number)
-        fetchDogImage()
+        fetchDogImage(number)
     })
 }
 
-/*function displayDogImage(){
-    $(".doge").forEach().replaceWith(
-        `<img class="doge" src="${responseJSON}"alt="doggie"></img>`
+function displayDogImage(responseJson){
+    $(".doge").replaceWith(
+        `<section class="doge">
+            <h2>Zoinks! That's a lot of dogs!</h2><br>
+            <img src="${responseJson.message}" alt="doggie>
+        </section>`
     )
 }
-
-function funTitle(){
-    $(".doge").removeClass(hidden)
-}
-*/
 storeNumber()
