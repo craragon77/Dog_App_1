a65f6ab50fff8f88a42ba317a65e1073107a46be
Chris Aragon <aragonchristopherray@gmail.com>
2020-01-13 17:12:00 -0500

    i added an images method to make this darn array

--- dog_app_1.js
+++ dog_app_1.js
@@ -3,9 +3,8 @@
 function fetchDogImage(number){
     fetch('https://dog.ceo/api/breeds/image/random/' + number)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson))
-    .then(responseJson => displayDogImage(responseJson))    
-    .catch(alert("Ruh_Roh! Something went wrong! Maybe try again later"));
+    .then(responseJson => console.warn(responseJson) || displayDogImage(responseJson))   
+    .catch(e => console.warn(e) || console.log("Ruh_Roh! Something went wrong! Maybe try again later"));
 }
 
 function storeNumber(){
@@ -14,15 +13,25 @@ function storeNumber(){
         let number = $(".number").val() === "" ? 3 : $(".number").val();
         console.log(number)
         fetchDogImage(number)
+        images()
     })
 }
 
 function displayDogImage(responseJson){
+    //console.log(responseJson.message)
     $(".doge").replaceWith(
         `<section class="doge">
             <h2>Zoinks! That's a lot of dogs!</h2><br>
-            <img src="${responseJson.message}" alt="doggie>
+            <div class="dog_list"></div>
         </section>`
     )
+}
+
+function images(){
+    for (let i = 0; i < responseJson.length; i++){
+        $(".dog_list").append(
+            <img scr="${responseJson.message}" alt="doggie"></img>
+        )
+    }
 }
 storeNumber()
