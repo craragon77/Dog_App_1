8df9a63bb9e3e5dff06dcc5c31b37243e2de9814
Chris Aragon <aragonchristopherray@gmail.com>
2020-01-14 12:34:00 -0500

    i have a hunch i am getting real close

--- dog_app_1.js
+++ dog_app_1.js
@@ -3,8 +3,8 @@
 function fetchDogImage(number){
     fetch('https://dog.ceo/api/breeds/image/random/' + number)
     .then(response => response.json())
-    .then(responseJson => console.warn(responseJson) || displayDogImage(responseJson))   
-    .catch(e => console.warn(e) || console.log("Ruh_Roh! Something went wrong! Maybe try again later"));
+    .then(responseJson => console.warn(responseJson) || displayDogImage(responseJson))
+    .catch(e => console.warn(e) || alert("Ruh_Roh! Something went wrong! Maybe try again later"));
 }
 
 function storeNumber(){
@@ -13,25 +13,25 @@ function storeNumber(){
         let number = $(".number").val() === "" ? 3 : $(".number").val();
         console.log(number)
         fetchDogImage(number)
-        images()
     })
 }
-
+//the issue appears to be that by hardcoding the individual image, you can only get them one at a time
 function displayDogImage(responseJson){
     //console.log(responseJson.message)
     $(".doge").replaceWith(
         `<section class="doge">
-            <h2>Zoinks! That's a lot of dogs!</h2><br>
-            <div class="dog_list"></div>
+            <h2>Zoinks! That's a lot of dogs</h2>
         </section>`
     )
 }
-
-function images(){
-    for (let i = 0; i < responseJson.length; i++){
-        $(".dog_list").append(
-            <img scr="${responseJson.message}" alt="doggie"></img>
+function dogLoop(responseJson){
+    for (let i = 0; i <= responseJson.length; i++){
+        $(".doge").append(
+            `<img src="${responseJson.message[i]}" alt="placeholder>`
         )
     }
 }
+
+
+
 storeNumber()
